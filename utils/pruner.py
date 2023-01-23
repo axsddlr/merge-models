71805640369b4462cb20061dc69cc9870a6fc9ce
Andre Saddler <dresadd09691@gmail.com>
2023-01-23 00:51:54 -0500

    utils/pruner: proper states checks

--- utils/pruner.py
+++ utils/pruner.py
@@ -9,9 +9,17 @@ def prune_checkpoint(old_state):
         print("Removing optimizer states from checkpoint")
     if "global_step" in old_state:
         print(f"This is global step {old_state['global_step']}.")
-    old_state = pruned_checkpoint['state_dict'].copy()
-    new_state = dict()
-    for key in old_state:
-        new_state[key] = old_state[key].half()
-    pruned_checkpoint['state_dict'] = new_state
+    state_key = None
+    if 'state_dict' in old_state:
+        state_key = 'state_dict'
+    elif 'model_state' in old_state:
+        state_key = 'model_state'
+    # add more checks for other keys where the model's state might be stored
+
+    if state_key and state_key in pruned_checkpoint:
+        old_state = pruned_checkpoint[state_key].copy()
+        new_state = dict()
+        for key in old_state:
+            new_state[key] = old_state[key].half()
+        pruned_checkpoint[state_key] = new_state
     return pruned_checkpoint
