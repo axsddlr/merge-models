d748ebed8026762f72fe623a06fa7f6fad737e18
Andre Saddler <dresadd09691@gmail.com>
2023-01-23 00:36:43 -0500

    pruner_models: delete saved pruned.ckpt if its same size as original

--- prune_models.py
+++ prune_models.py
@@ -12,6 +12,11 @@ device = args.device
 
 
 def prune_it(checkpoint_path):
+    """
+    It loads a checkpoint, prunes it, and saves it
+
+    :param checkpoint_path: The path to the checkpoint file
+    """
     print(f"Pruning checkpoint from path: {checkpoint_path}")
     size_initial = os.path.getsize(checkpoint_path)
     checkpoint = torch.load(checkpoint_path, map_location=device)
@@ -22,8 +27,13 @@ def prune_it(checkpoint_path):
     print(f"Saving pruned checkpoint at: {fn}")
     torch.save(pruned, fn)
     newsize = os.path.getsize(fn)
-    MSG = f"New ckpt size: {newsize*1e-9:.2f} GB. " + \
-          f"Saved {(size_initial - newsize)*1e-9:.2f} GB by removing optimizer states"
-    print(MSG)
+    if newsize >= size_initial:
+        os.remove(fn)  # deleting the new checkpoint because it is not smaller than the original
+        print("No changes were made, original checkpoint is kept")
+    else:
+        MSG = f"New ckpt size: {newsize * 1e-9:.2f} GB. " + \
+              f"Saved {(size_initial - newsize) * 1e-9:.2f} GB by removing optimizer states"
+        print(MSG)
+
 
 prune_it(ckpt)
